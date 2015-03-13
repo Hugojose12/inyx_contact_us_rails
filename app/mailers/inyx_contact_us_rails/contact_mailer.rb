99dd3eda51cb9f38109a646fe1e982cb7cf2511a
Gabriel Morillo <gbrlmrllo@gmail.com>
2015-03-13 17:31:08 -0430

    Update contact_mailer.rb

--- app/mailers/inyx_contact_us_rails/contact_mailer.rb
+++ app/mailers/inyx_contact_us_rails/contact_mailer.rb
@@ -2,7 +2,7 @@ module InyxContactUsRails
   class ContactMailer < ActionMailer::Base
     def contact(client)
 	    @client = client
-	    mail(from: "no-reply@inyxtech.com", to: InyxContactUsRails.mailer_to, subject: @client[:subject])
+	    mail(from: InyxContactUsRails.mailer_from, to: InyxContactUsRails.mailer_to, subject: @client[:subject])
 	  end
   end
 end
